781e427158b68e7f84d548b6d45f30ea580bf6dd
github username <github email address>
2019-08-19 14:54:34 -0400

    schemas and resolvers

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const graphqlHttp = require('express-graphql')
+const { buildSchema } = require('graphql')
 
 // call express as a function to create app
 const app = express()
@@ -7,10 +9,32 @@ const app = express()
 // add bodyParser middleware to parse incoming JSON bodies
 app.use(bodyParser.json())
 
-// listen to get requests
-app.get('/', (req, res, next) => {
-  res.send('Hello World!')
-})
+app.use('/graphql', graphqlHttp({
+  schema: buildSchema(`
+    type RootQuery {
+      events: [String!]
+    }
+
+    type RootMutation {
+      createEvent(name: String): String
+    }
+
+    schema {
+      query: RootQuery
+      mutation: RootMutation
+    }
+  `),
+  rootValue: {
+    events: () => {
+      return ['Romantic Cooking', 'Sailing', 'All-Night Coding']
+    },
+    createEvent: (args) => {
+      const eventName = args.name
+      return eventName
+    }
+  },
+  graphiql: true
+}))
 
 // listen on port 3000
 app.listen(3000)
